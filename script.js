af2b9ef2ea19f9d11172ccb46acde1cf0847171f
Allyson Mitchem <allysonmitchem@gmail.com>
2023-06-27 15:24:16 -0600

    set theme in local storage

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ function darkMode(){
     toggleIcon.children[0].textContent = 'Dark Mode';
     toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
     imageMode('dark');
-
 }
 
 // Light Mode Styles
@@ -29,8 +28,7 @@ function lightMode(){
     textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     toggleIcon.children[0].textContent = 'Light Mode';
     toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
- imageMode('light');
-
+    imageMode('light');
 }
 
 
@@ -38,12 +36,24 @@ function lightMode(){
 function switchTheme(event) {
     if (event.target.checked){
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
         darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
         lightMode();
     }
 }
 
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Check Local Storage for Theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark'){
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
